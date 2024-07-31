e70091400cc4799421e5379eecd8b016a9ec039f
yslaoui <yslaouiyassine@gmail.com>
2024-07-31 17:10:17 +0100

    Adding a /api/students/{studentId} endpoint that returns single student objects

--- src/main/java/com/example/rest/controller/StudentRestController.java
+++ src/main/java/com/example/rest/controller/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.example.rest.controller;
 
 import com.example.rest.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +13,25 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+    List<Student> students;
+
+    @PostConstruct
+    public void load() {
+        this.students  = new ArrayList<>();
+        this.students.add(new Student("Maria", "Fitoussi"));
+        this.students.add(new Student("Anna", "kamel"));
+        this.students.add(new Student("Kamala", "Trump"));
+
+    }
 
     @GetMapping("/students")
     public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
-        students.add(new Student("Maria", "Fitoussi"));
-        students.add(new Student("Anna", "kamel"));
-        students.add(new Student("Kamala", "Trump"));
-        return students;
+        this.load();
+        return this.students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return this.students.get(studentId);
     }
 }
